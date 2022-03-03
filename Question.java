c24a31117cd5b23e034f302bd945ca04fac77c55
Matthew Micale <69608984+Matthew-Micale@users.noreply.github.com>
2022-03-03 15:25:22 -0500

    start of question class

--- Question.java
+++ Question.java
@@ -1,4 +1,18 @@
 public class Question
 {
   
+	public String text;
+	public int weight;
+	public Trait trait1;
+	public Trait trait2;
+	
+	public Question(String t, int w, Trait t1, Trait t2)
+	{
+		
+		text = t;
+		weight = w;
+		trait1 = t1;
+		trait2 = t2;
+	}
+	
 }
