381445d7d5fc5375ba89a9bacfdf5f5bf470d875
Nicky Nath <Nicky_Nath@student.uml.edu>
2022-03-24 20:24:03 -0400

    Changed and moved the CSV reader

--- driver.java
+++ driver.java
@@ -1,4 +1,10 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.StringTokenizer;
+
+
 
 public class driver
 
@@ -63,6 +69,20 @@ public static void main(String[] args)
 			i++;
 		}
 	}
+	
+	try {
+	      //csv file containing data
+	      String strFile = "C:/Users/rsaluja/CMS_Evaluation/Drupal_12_08_27.csv";
+	      CSVReader reader = new CSVReader(new FileReader(strFile));
+	      String [] nextLine;
+	      int lineNumber = 0;
+	      while ((nextLine = reader.readNext()) != null) {
+	        lineNumber++;
+	        System.out.println("Line # " + lineNumber);
+
+	        // nextLine[] is an array of values from the line
+	        System.out.println(nextLine[4] + "etc...");
+	      }
 }
 
 }
